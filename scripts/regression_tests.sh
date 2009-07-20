5ac5af402e5b50ae4ae16a45a46c2e37c884b12d
Pjotr Prins <pjotr.public01@thebird.nl>
2009-07-20 10:03:51 +0200

    Relacing char codes with const for marker location LRMU

--- scripts/regression_tests.sh
+++ scripts/regression_tests.sh
@@ -3,7 +3,8 @@
 echo "Starting regression tests"
 cd ../test
 ls -l
-valgrind ../src/mqm -T=0 -V > ../test/MQM_test0.txt
+# valgrind ../src/mqm -T=0 -V > ../test/MQM_test0.txt
+../src/mqm -T=0 -V > ../test/MQM_test0.txt
 
 echo "Finalized regression tests"
 exit 0
